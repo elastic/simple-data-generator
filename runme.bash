41c484deae7e1d2ab0c4b13c5f9dc47a7ec30028
AJ Pahl <aj.pahl@elastic.co>
2019-10-01 19:28:48 -0400

    updated APM and High Level Rest Client versions

--- runme.bash
+++ runme.bash
@@ -1,1 +1,1 @@
-java -jar build/libs/simple-data-generator-all-1.0-SNAPSHOT.jar examples/mortgage-data.yml
+java -jar build/libs/simple-data-generator-all-1.0-SNAPSHOT.jar examples/disk-fill-workload.protect
